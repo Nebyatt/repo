0766161292292280c5cd78197ad40ef2b30d3dee
Nebyatt <129296133+Nebyatt@users.noreply.github.com>
2024-02-05 21:32:07 -0500

    ESPGAME.java

--- ESPGAME.java
+++ ESPGAME.java
@@ -1,8 +1,8 @@
 /*
  * Class: CMSC203 
- * Instructor: Khansan Monshi
+ * Instructor: Khanan Monshi
  * Description: (Give a brief description for each Class)
- * Due: 02/04/2024
+ * Due: 02/05/2024
  * Platform/compiler:
  * I pledge that I have completed the programming assignment 
 * independently. I have not copied the code from a student or   * any source. I have not given my code to any student.
@@ -35,63 +35,68 @@ public class ESPGAME
 		dueDate = input.nextLine();
 	
 		System.out.println("CMSC203 Assignment 1: test your ESP skills!");
-		// input.nextLine();
+		input.nextLine();
 		
 		final String COLOR_RED = "Red";
 	    final String COLOR_GREEN = "Green";
 	    final String COLOR_BLUE = "Blue";
 	    final String COLOR_ORANGE = "Orange";
-	    final String COLOR_YELLOW = "Yellow";
+	    final String COLOR_YELLOW = "Yellow"; 
+	    final String WRONG_COLOR = "";
 	    
 	    int rightGuess = 0;
 
-	    for (int round = 1; round <= 11; round++) 
+	    for (int round = 1; round < 12; round++)
 	    {
-	    	int randomNumber = (int)(Math.random() * 6);
-   
-	    	String selectedColor = "";
-	    	if (randomNumber == 0) 
-	    	{
-	    		selectedColor = COLOR_RED;
-	    	} 
-	    	else if (randomNumber == 1) 
-	    	{
-	    		selectedColor = COLOR_GREEN;
-	    	} 
-	    	else if (randomNumber == 2) 
-	    	{
-	    		selectedColor = COLOR_BLUE;
-	    	} 
-	    	else if (randomNumber == 3) 
-	    	{
-	    		selectedColor = COLOR_ORANGE;
-	    	} 
-	    	else if (randomNumber == 4) 
-	    	{
-	    		selectedColor = COLOR_YELLOW;
-	    	} 
-	    
-	    
-	    System.out.println("\nRound " + round);
-    	System.out.println("I am thinking of a color.");
-        System.out.println("Is it Red, Green, Blue, Orange, or Yellow?");
-        System.out.print("Enter your guess: ");
-        String guesses = input.nextLine();
-
-        System.out.println("I was thinking of " + selectedColor + ".");
-        
-        
-        
-        if (guesses.equalsIgnoreCase(selectedColor)) {
-            rightGuess++;
-        }
-        
-      }
-	    System.out.println("\nGame Over\n");
-        System.out.println("You guessed " + rightGuess + " out of 11 colors correctly.");
-        System.out.println("Student Name: " + name);
-        System.out.println("User Description: " + description);
-        System.out.println("Due Date: " + dueDate);
+	       // Generate a random number from 0 to 4
+	       int randomNumber = random.nextInt(5);
+	       String color = "";
+	       
+	       switch (randomNumber) 
+	       {
+	       		case 0:
+	                color = COLOR_RED;
+	                break;
+	            case 1:
+	                color = COLOR_GREEN;
+	                break;
+	            case 2:
+	                color = COLOR_BLUE;
+	                break;
+	            case 3:
+	                color = COLOR_ORANGE;
+	                break;
+	            case 4:
+	            	color = COLOR_YELLOW;
+	                break;
+	            default:
+	            	System.out.println("\nRound " + round);
+	    	        System.out.println("I am thinking of a color.");
+	    		    System.out.println("Is it Red, Green, Blue, Orange, or Yellow?");
+	    		    System.out.print("Enter your guess: ");
+	    	        String userGuess = input.nextLine();
+	    	        System.out.println("I was thinking of: " + color);
+	        }
+	       
+	        System.out.println("\nRound " + round);
+	        System.out.println("I am thinking of a color.");
+		    System.out.println("Is it Red, Green, Blue, Orange, or Yellow?");
+		    System.out.print("Enter your guess: ");
+	        String userGuess = input.nextLine();
+	        System.out.println("I was thinking of: " + color);
+	        
+	        if (userGuess.equalsIgnoreCase(color))
+	        {
+	        	rightGuess++;
+	        }
+																	
+	    }
+	        
+	     System.out.println("\nGame Over\n");
+	     System.out.println("You guessed " + rightGuess + " out of 11 colors correctly.");
+	     System.out.println("Student Name: " + name);
+	     System.out.println("User Description: " + description);
+	     System.out.println("Due Date: " + dueDate);
 	  
 	}
 
